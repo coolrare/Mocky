a884bcb6a2e19bacb260902f312ebe9403aa504c
Will <doggy.huang@gmail.com>
2020-03-22 00:25:09 +0800

    加入專案檔案。

---
+++ Mocky/Controllers/DelayController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Mocky.Controllers
+{
+    [RoutePrefix("api/delay")]
+    public class DelayController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        [Route("{ms}")]
+        public async Task<string> GetAsync(int ms = 0)
+        {
+            await Task.Delay(ms);
+
+            return $"Delay {ms}ms";
+        }
+
+        [HttpGet]
+        [Route("~/")]
+        public IHttpActionResult Home(int ms = 0)
+        {
+            return Redirect("https://blog.miniasp.com");
+        }
+    }
+}
---
+++ Mocky/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mocky.WebApiApplication" Language="C#" %>
